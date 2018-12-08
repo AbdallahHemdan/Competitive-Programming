bdf5dd68684248af86d6063fb50363476217e562
AbdallahHemdan <40190772+AbdallahHemdan@users.noreply.github.com>
2018-12-08 23:31:32 +0200

    Delete IsPrime.cpp

--- IsPrime.cpp
+++
@@ -1,9 +0,0 @@
-// Check if The Number Is Prime Or Not
-bool IsPrime(int n){
-    if(n==2) return true ; 
-    if(n<2 || n%2==0) return false ; 
-    for(int i=3 ; i*i<=n;i+=2)
-        if(n%i==0)
-            return false ; 
-    return true ; 
-}
