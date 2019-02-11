22364701fa0de6ca6dd58a48371f59e784dbf269
Abdallah Hemdan <40190772+AbdallahHemdan@users.noreply.github.com>
2019-02-11 06:42:54 +0200

    Update IsPrime.cpp

--- Math/IsPrime.cpp
+++ Math/IsPrime.cpp
@@ -1,8 +1,8 @@
 // Check if The Number Is Prime Or Not
-bool IsPrime(int n){
+bool IsPrime(ll n){
     if(n==2) return true ; 
     if(n<2 || n%2==0) return false ; 
-    for(int i=3 ; i*i<=n;i+=2)
+    for(ll i=3 ; i*i<=n;i+=2)
         if(n%i==0)
             return false ; 
     return true ; 
