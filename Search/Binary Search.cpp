72d780014227d1b58760a6c5c6be1c6ba38b2106
AdelRizq <40351413+AdelRizq@users.noreply.github.com>
2019-10-16 21:43:47 +0200

    update BS function
    modify the BS function to suits the monotonic function

--- Search/Binary Search.cpp
+++ Search/Binary Search.cpp
@@ -6,14 +6,21 @@ bool Ok(int m) {
 
 // Binary search on the minimum value fulfilling a certain criteria
 int binarySearch() {
-	int l = 0, r = 1e9;
+	int l = 0, r = 1e9, ans, ans = -1;
 
-    while (l < r) {
-        int mid = l + (r - l) / 2;
+    while (l <= r) {
+        mid = l + (r - l) / 2; // take care of overflow 
 
+	// if the function 0 0 0 0 1 1 1 1 1
         if (Ok(mid))
-            r = mid;
+            r = mid - 1, ans = mid;
         else
             l = mid + 1;
+	    
+	// if the funciton 1 1 1 1 0 0 0 0 0
+	/* if (OK(mid))
+		l = mid + 1, ans = mid;
+	   else 
+	   	r = mid - 1;
     }
 }
